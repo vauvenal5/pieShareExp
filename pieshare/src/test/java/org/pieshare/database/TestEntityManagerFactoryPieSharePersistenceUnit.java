9fb6b6460ebc855b06a84438748bee71d2a1f5be
Richard Leopold <richy.leopold@outlook.com>
2013-07-24 13:34:35 +0200

    added JPA Test, add entity and read back entity

--- pieshare/src/test/java/org/pieshare/database/TestEntityManagerFactoryPieSharePersistenceUnit.java
+++ pieshare/src/test/java/org/pieshare/database/TestEntityManagerFactoryPieSharePersistenceUnit.java
@@ -4,9 +4,12 @@
  */
 package org.pieshare.database;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
@@ -22,9 +25,17 @@ public class TestEntityManagerFactoryPieSharePersistenceUnit
 	private EntityManagerFactory emf;
 	private EntityManager em;
 
+	private TestUserEntity testUser1;
+	private TestUserEntity testUser2;
+
 	@Before
 	public void initEmfAndEm()
 	{
+		testUser1 = new TestUserEntity();
+		testUser1.setName("User1");
+		testUser2 = new TestUserEntity();
+		testUser2.setName("testUser2");
+
 		emf = Persistence.createEntityManagerFactory("pieSharePersistenceUnit");
 		em = emf.createEntityManager();
 	}
@@ -35,8 +46,24 @@ public class TestEntityManagerFactoryPieSharePersistenceUnit
 		em.close();
 	}
 
-	@Test
-	public void emptyTest()
+	@SuppressWarnings("unchecked")
+    @Test
+    public void insertAndRetrieve()
 	{
+		em.getTransaction().begin();
+		em.persist(testUser1);
+		em.persist(testUser2);
+		em.getTransaction().commit();
+
+		final List<TestUserEntity> list = em.createQuery("select p from TestUserEntity p")
+				.getResultList();
+
+		assertEquals(2, list.size());
+		for (TestUserEntity current : list)
+		{
+			final String firstName = current.getName();
+			System.out.println(firstName + ": GeneratedID: " + current.getId());
+			assertTrue(firstName.equals("User1") || firstName.equals("testUser2"));
+		}
 	}
 }
