d992c4173eca9c21c085357135e67f0bb1106e4c
Richard Leopold <richy.leopold@outlook.com>
2013-08-02 09:12:36 +0200

    moved folderConentReader, was in test directory before FileWatcherTest is still not running :(

--- pieshare/src/main/java/org/pieshare/service/filewatcher/FolderContentReader.java
+++ pieshare/src/main/java/org/pieshare/service/filewatcher/FolderContentReader.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.pieshare.filewatcher;
+package org.pieshare.service.filewatcher;
 
 import java.util.List;
 import org.pieshare.domain.PieceOfPie;
--- pieshare/src/test/java/org/pieshare/filewatcher/TestFileWatcher.java
+++ pieshare/src/test/java/org/pieshare/filewatcher/TestFileWatcher.java
@@ -3,8 +3,12 @@
  * and open the template in the editor.
  */
 package org.pieshare.filewatcher;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import junit.framework.Assert;
 import org.junit.After;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -14,6 +18,11 @@ import org.pieshare.service.event.EventCallback;
 import org.pieshare.service.filewatcher.FileWatcherEvent;
 import org.pieshare.service.filewatcher.FileWatcherService;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.pieshare.domain.PieceOfPie;
+import org.pieshare.service.event.EventBaseService;
+import org.pieshare.service.filewatcher.IFileWatcherService;
 
 /**
  *
@@ -21,35 +30,50 @@ import org.mockito.Mockito;
  */
 public class TestFileWatcher
 {
+
 	private int eventCount = 0;
-	
+
 	@Before
 	public void startUp()
 	{
 	}
-	
+
 	@After
 	public void cleanUp()
 	{
-	
 	}
-	
-	
+
 	@Test
 	public void testFileWatcher()
 	{
-			
-		String testPath = "../";
+
+	Assert.fail("Not implemented");
 		
+		String testPath = "../";
+		ExecutorService executorService = Executors.newCachedThreadPool();
+
+		EventBaseService mockedEventBaseServie = Mockito.mock(EventBaseService.class);
+		
+		Mockito.doAnswer(new Answer()
+		{
+			@Override
+			public Object answer(InvocationOnMock invocation) throws Throwable
+			{
+				fileWatcherListener();
+				return null;
+			}
+		}).when(mockedEventBaseServie).fireEvent(FileWatcherEvent.class, Mockito.anyObject(), Mockito.anyCollection().toArray());
+
 		FileWatcherService fileWatcher = new FileWatcherService();
 		fileWatcher.setDirectory(testPath);
-		fileWatcher.addFileChangeEventListener(this);
-		
-		fileWatcher.run();
-		
+		//fileWatcher.addFileChangeEventListener(this);
+		fileWatcher.setEventBaseService(mockedEventBaseServie);
+
+		executorService.execute(fileWatcher);
+
 		File f1 = new File(testPath, ".testFile");
-		
-		if(!f1.exists())
+
+		if (!f1.exists())
 		{
 			try
 			{
@@ -61,20 +85,20 @@ public class TestFileWatcher
 				assertFalse(true);
 			}
 		}
-		
-		assertEquals(eventCount, 1);
-		
+
+		assertEquals(1, eventCount);
+
 		//Stopps repeating the filewatcher after next interation.
 		fileWatcher.stopRunning();
-		
+
 		f1.delete();
-		
-		assertEquals(eventCount, 2);
-		
+
+		assertEquals(2, eventCount);
+
 		assertEquals(f1.exists(), false);
-		
+
 	}
-	
+
 	@EventCallback(eventClass = FileWatcherEvent.class)
 	public void fileWatcherListener()
 	{
