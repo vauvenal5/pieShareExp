8215330bd702df79221e57aac06bac2d417704d7
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2013-11-28 18:33:08 +0100

    piePlate structure

---
+++ pietools/pieplate/src/main/java/org/pietools/pieplate/controller/core/PiePlateController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pietools.pieplate.controller.core;
+
+import org.pietools.pieplate.controller.api.IPiePlateController;
+
+/**
+ *
+ * @author vauvenal5
+ */
+public class PiePlateController implements IPiePlateController
+{
+    
+}
