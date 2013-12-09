afd6274ab66b008e01d7c751d14995ea86ef4d6b
Svetoslav Videnov <videnov.svetoslav@gmail.com>
2013-12-09 15:48:25 +0100

    added broadcast interface

---
+++ pietools/pieplate/src/main/java/org/pietools/pieplate/service/api/IBroadcastService.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.pietools.pieplate.service.api;
+
+/**
+ *
+ * @author vauvenal5
+ */
+public interface IBroadcastService
+{
+    void eatMe(int port);
+}
